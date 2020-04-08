3fe2fe2c9b100181723c249d2578e8b7d2e83508
Peter Mullen <platinum95@gmail.com>
2020-04-08 03:43:37 +0100

    Add basic mqtt listener to forward data to the database

---
+++ mqttListener.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import paho.mqtt.client as mqtt
+import json
+import mysql.connector
+from config import TempMonConfig as CONFIG
+import time
+
+def on_connect( client, userdata, flags, rc ):
+    global tempMonDb
+    print( "We did it! rc:" + str( rc ) )
+    print( "Creating DB connection" )
+    tempMonDb = mysql.connector.connect(
+            host=CONFIG.SQLDbHost,
+            user=CONFIG.SQLDbUser,
+            database=CONFIG.SQLDbName,
+            passwd=CONFIG.SQLDbPassword )
+    print( "DB: " + str( tempMonDb ) )
+    
+def on_message( client, userdata, msg ):
+    global tempMonDb
+    if( msg.topic == "test/message" ):
+        y = json.loads( msg.payload )
+        temp = float( y[ "temp" ] )
+        hum = float( y[ "hum" ] )
+        eTime = time.time()
+        print( "Temperature: " + str( temp ) + ", humidity: " + str( hum ) )
+        sql = "INSERT INTO SensorData (temp, humidity, timestamp) VALUES(%s, %s, %s)"
+        vals = ( temp, hum, eTime )
+        cursor = tempMonDb.cursor()
+        cursor.execute( sql, vals )
+        tempMonDb.commit()
+
+    else:
+        print( "Received: " + str( msg.payload ) )
+
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print( "Subscribed: " + str( mid ) + " " + str( granted_qos ) )
+
+def on_log(mqttc, obj, level, string):
+    print(string)
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_subscribe = on_subscribe
+client.on_log = on_log
+
+client.connect( "localhost", 1883, 60 )
+
+client.subscribe( "test/message" )
+
+client.loop_forever()
